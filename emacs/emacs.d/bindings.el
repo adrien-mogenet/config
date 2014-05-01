3b82f1da2937f76f2a511be23186ffd636a28629
Adrien Mogenet <adrien.mogenet@gmail.com>
2014-05-01 18:50:54 +0200

    Resize emacs windows

--- emacs/emacs.d/bindings.el
+++ emacs/emacs.d/bindings.el
@@ -3,60 +3,88 @@
 ;; BINDINGS
 ;; ----------------------------------------------------------------------------
 
+;; Define functions to resize windows
+
+(defun win-resize-top-or-bot ()
+  "Figure out if the current window is on top, bottom or in the
+middle"
+  (let* ((win-edges (window-edges))
+         (this-window-y-min (nth 1 win-edges))
+         (this-window-y-max (nth 3 win-edges))
+         (fr-height (frame-height)))
+    (cond
+     ((eq 0 this-window-y-min) "top")
+     ((eq (- fr-height 1) this-window-y-max) "bot")
+     (t "mid"))))
+
+(defun win-resize-left-or-right ()
+  "Figure out if the current window is to the left, right or in the
+middle"
+  (let* ((win-edges (window-edges))
+         (this-window-x-min (nth 0 win-edges))
+         (this-window-x-max (nth 2 win-edges))
+         (fr-width (frame-width)))
+    (cond
+     ((eq 0 this-window-x-min) "left")
+     ((eq (+ fr-width 4) this-window-x-max) "right")
+     (t "mid"))))
+
+(defun win-resize-enlarge-horiz ()
+  (interactive)
+  (cond
+   ((equal "top" (win-resize-top-or-bot)) (enlarge-window -1))
+   ((equal "bot" (win-resize-top-or-bot)) (enlarge-window 1))
+   ((equal "mid" (win-resize-top-or-bot)) (enlarge-window -1))
+   (t (message "nil"))))
+
+(defun win-resize-minimize-horiz ()
+  (interactive)
+  (cond
+   ((equal "top" (win-resize-top-or-bot)) (enlarge-window 1))
+   ((equal "bot" (win-resize-top-or-bot)) (enlarge-window -1))
+   ((equal "mid" (win-resize-top-or-bot)) (enlarge-window 1))
+   (t (message "nil"))))
+
+(defun win-resize-enlarge-vert ()
+  (interactive)
+  (cond
+   ((equal "left" (win-resize-left-or-right)) (enlarge-window-horizontally -1))
+   ((equal "right" (win-resize-left-or-right)) (enlarge-window-horizontally 1))
+   ((equal "mid" (win-resize-left-or-right)) (enlarge-window-horizontally -1))))
+
+(defun win-resize-minimize-vert ()
+  (interactive)
+  (cond
+   ((equal "left" (win-resize-left-or-right)) (enlarge-window-horizontally 1))
+   ((equal "right" (win-resize-left-or-right)) (enlarge-window-horizontally -1))
+      ((equal "mid" (win-resize-left-or-right)) (enlarge-window-horizontally 1))))
+
+
 ;; BINDINGS :: isearch
 (global-set-key [(control f)] 'isearch-forward-regexp)  ; search regexp
 (global-set-key [(control r)] 'query-replace-regexp)    ; replace regexp
-(define-key
- isearch-mode-map
- [(control n)]
- 'isearch-repeat-forward)                              ; next occurence
-(define-key
- isearch-mode-map
- [(control p)]
- 'isearch-repeat-backward)                             ; previous occurence
-(define-key
- isearch-mode-map
- [(control z)]
- 'isearch-cancel)                                      ; quit and go back to start point
-(define-key
- isearch-mode-map
- [(control f)]
- 'isearch-exit)                                        ; abort
-(define-key
- isearch-mode-map
- [(control r)]
- 'isearch-query-replace)                               ; switch to replace mode
-(define-key
- isearch-mode-map
- [S-insert]
- 'isearch-yank-kill)                                   ; paste
-(define-key
- isearch-mode-map
- [(control e)]
- 'isearch-toggle-regexp)                               ; toggle regexp
-(define-key
- isearch-mode-map
- [(control l)]
- 'isearch-yank-line)                                   ; yank line from buffer
-(define-key
- isearch-mode-map
- [(control w)]
- 'isearch-yank-word)                                   ; yank word from buffer
-(define-key
- isearch-mode-map
- [(control c)]
- 'isearch-yank-char)                                   ; yank char from buffer
+(define-key isearch-mode-map [(control n)] 'isearch-repeat-forward) ; next occurence
+(define-key isearch-mode-map [(control p)] 'isearch-repeat-backward); previous occurence
+(define-key isearch-mode-map [(control z)] 'isearch-cancel)         ; quit and go back to start point
+(define-key isearch-mode-map [(control f)] 'isearch-exit)           ; abort
+(define-key isearch-mode-map [(control r)] 'isearch-query-replace)  ; switch to replace mode
+(define-key isearch-mode-map [S-insert] 'isearch-yank-kill)         ; paste
+(define-key isearch-mode-map [(control e)] 'isearch-toggle-regexp)  ; toggle regexp
+(define-key isearch-mode-map [(control l)] 'isearch-yank-line)      ; yank line from buffer
+(define-key isearch-mode-map [(control w)] 'isearch-yank-word)      ; yank word from buffer
+(define-key isearch-mode-map [(control c)] 'isearch-yank-char)      ; yank char from buffer
 
 ;; BINDINGS :: misc
-(global-set-key [(control z)] 'undo)                    ; undo
-(global-set-key [C-home] 'beginning-of-buffer)          ; go to the beginning of buffer
-(global-set-key [C-end] 'end-of-buffer)                 ; go to the end of buffer
-(global-set-key [(meta g)] 'goto-line)                  ; goto line #
-(global-set-key [(control tab)] 'other-window)          ; Ctrl-Tab = Next buffer
-(global-set-key [C-S-iso-lefttab]
-                '(lambda () (interactive)
-                   (other-window -1)))                  ; Ctrl-Shift-Tab = Previous buffer
-(global-set-key [f12] 'replace-string)
+(global-set-key [(control z)] 'undo)           ; undo
+(global-set-key [C-home] 'beginning-of-buffer) ; go to the beginning of buffer
+(global-set-key [C-end] 'end-of-buffer)        ; go to the end of buffer
+(global-set-key [(meta g)] 'goto-line)         ; goto line #
+
+;; BINDINGS :: resizing windows
+(global-set-key (kbd "C-x C-<up>") 'win-resize-enlarge-horiz)
+(global-set-key (kbd "C-x C-<down>") 'win-resize-minimize-horiz)
+(global-set-key (kbd "C-x C-<left>") 'win-resize-enlarge-vert)
+(global-set-key (kbd "C-x C-<right>") 'win-resize-minimize-vert)
 
 ;; BINDINGS :: for use within terminal
 (global-set-key (kbd "C-c <right>") 'windmove-right)
@@ -70,10 +98,6 @@
 (global-set-key [f9] 'next-error)
 (global-set-key [f10] 'recompile)
 (global-set-key [f11] 'compile)
-
-;; BINDINGS :: font size
-(global-set-key [(control +)] 'inc-font-size)
-(global-set-key [(control =)] 'reset-font-size)
 
 ;; BINDINGS :: ido
 (global-set-key [(control b)] 'ido-switch-buffer)
