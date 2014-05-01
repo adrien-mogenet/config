c9704fca3bec77b3aea0f1d19cfe9ebffa098cb7
Adrien Mogenet <adrien.mogenet@gmail.com>
2014-05-01 18:51:44 +0200

    Custom clojure settings for emacs

--- emacs/emacs.d/lang/clojure.el
+++ emacs/emacs.d/lang/clojure.el
@@ -27,3 +27,14 @@
 (add-hook 'cider-repl-mode-hook 'subword-mode)
 (add-hook 'cider-repl-mode-hook 'paredit-mode)
 (add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
+
+
+(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+(setq cider-repl-tab-command 'indent-for-tab-command)
+(setq cider-repl-pop-to-buffer-on-connect nil)
+(setq cider-popup-stacktraces nil)
+(setq cider-repl-popup-stacktraces t)
+(setq cider-auto-select-error-buffer t)
+(setq nrepl-hide-special-buffers t)
+(setq nrepl-buffer-name-separator "-")
+(setq nrepl-buffer-name-show-port t)
