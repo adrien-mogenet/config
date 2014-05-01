768e9630d17ea1e242d0f38d268767dceb1a8761
Adrien Mogenet <adrien.mogenet@gmail.com>
2014-05-01 16:33:42 +0200

    Custom linum fonts

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -14,3 +14,23 @@
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auto-save-file-name-transforms (quote ((".*" "~/.emacs.d/autosaves/\\1" t))))
+ '(backup-directory-alist (quote ((".*" . "~/.emacs.d/backups/"))))
+ '(column-number-mode t)
+ '(ecb-options-version "2.32")
+ '(mac-font-panel-mode nil)
+ '(mac-input-method-mode nil)
+ '(ns-antialias-text nil)
+ '(transient-mark-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(linum ((t (:inherit (shadow default) :background "#212121" :foreground "#444")))))
