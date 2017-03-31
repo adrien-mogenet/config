6e9059b944f02245d51d98896f70b15a8f239001
Adrien Mogenet <adrien.mogenet@gmail.com>
2017-03-31 16:54:48 +0200

    Update bash config

--- bash/bash.d/env.sh
+++ bash/bash.d/env.sh
@@ -2,9 +2,11 @@
 
 export PATH=${PATH}:/opt/local/Library/Frameworks/Python.framework/Versions/2.7/bin/:/usr/local/Cellar/emacs/24.5/bin/emacs
 export PATH=${PATH}:/Library/TeX/texbin
+export PATH=${PATH}:/Applications/Octave.app/Contents/Resources/usr/bin/
 export NAS=192.168.1.16
 
 # This avoids adding `._foobar` files within MacOSX tarballs
 export COPYFILE_DISABLE=1
 
-export KAPACITOR_URL="https://${KAPACITOR_USERNAME}:${KAPACITOR_PASSWORD}@futureboy-d28e6515.influxcloud.net:9092"
+source ~/.bash.d/passwords.sh
+export GOPATH=/Users/adrien/Development/go/
